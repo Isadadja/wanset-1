bd0b927faffadd7358b9da4fd67d974156ae6776
isadadja <106791139+Isadadja@users.noreply.github.com>
2022-06-03 15:04:59 +0700

    Upgrade

--- reffo.php
+++ reffo.php
@@ -1,5 +1,5 @@
 <?php
-//MASUKKAN KODE REFF
-$reff = "fQRvUn";
+//https://app.oneaset.co.id/finance/Finance/LandingPage?channel=web_OneAset_activity_financeinvite&referrerCode=ITXWZ&source=outside&ad=Ym09MiZjcD01Jmd1PW51bGwmdWM9MTAmdWU9MCZ1ZD02NDQ3MTY=
+$reff = "ITXWZ";
 
 ?>
